7984bfda83f077cb231ecfd9eee91a0e9f70a675
Yuvraj Singh <yssaini2004@gmail.com>
2023-08-07 06:59:54 +0530

    selecting by elements, node ids and classes

--- JavaScript_Basics/script.js
+++ JavaScript_Basics/script.js
@@ -1,5 +1,15 @@
-console.log("Hello world")
+console.log("Harry")
 
-document.body.firstElementChild 
-document.body.firstElementChild.childNodes 
-document.body.firstElementChild.children
+// let boxes = document.getElementsByClassName("box")
+// console.log(boxes)
+
+// boxes[2].style.backgroundColor = "red"
+
+// document.getElementById("redbox").style.backgroundColor = "red"
+
+// document.querySelector(".box").style.backgroundColor = "green";
+console.log(document.querySelectorAll(".box"))
+
+document.querySelectorAll(".box").forEach(e =>{
+    e.style.backgroundColor = "green";
+}) 
